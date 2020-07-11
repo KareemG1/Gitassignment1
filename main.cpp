15d2f4c6d6730f7a5ea60aee23c2702933241ed8
Kareem Gaber <kgaber1@aucegypt.edu>
2020-07-11 23:02:33 +0200

    message

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "Tamer.h"
+#include "ZiadSherbiny99.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+	tamerosman3();
+	ZiadSherbiny99();
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,4 @@
+all: m
+
+m: main.cpp Tamer.cpp ZiadSherbiny99.cpp
+	g++ main.cpp Tamer.cpp ZiadSherbiny99.cpp -o m
